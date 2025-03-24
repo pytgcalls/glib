e073ed645e28706f3e65c0eee88b615b9c8421a7
Laky64 <iraci.matteo@gmail.com>
2025-03-24 02:16:07 +0100

    Fixed Static Builds

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ require_venv
 
 GLIB_VERSION=$(get_version "glib")
 
-build_and_install https://github.com/GNOME/glib.git "$GLIB_VERSION" meson-static --buildtype=plain -Dtests=false
+build_and_install https://github.com/GNOME/glib.git "$GLIB_VERSION" meson-static --prefix="$(pwd)/glib/build/" --buildtype=plain -Dtests=false
 
 mkdir -p artifacts/lib
 mkdir -p artifacts/include
-run cp -r build_output/lib/*.a artifacts/lib/
-run cp -r build_output/include/glib-2.0/* artifacts/include/
-run cp build_output/lib/glib-2.0/include/glibconfig.h artifacts/include/
+cp -r "$(pwd)"/glib/build/lib/*.a artifacts/lib/
+cp -r "$(pwd)"/glib/build/include/glib-2.0/* artifacts/include/
+cp "$(pwd)"/glib/build/lib/glib-2.0/include/glibconfig.h artifacts/include/
